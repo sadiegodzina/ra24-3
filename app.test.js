18817e6f96b23ccf9d466bcfa0d06d6b43d3d5a2
Sadie Godzina <sadiegodzina@gmail.com>
2024-11-23 19:01:12 -0500

    changes

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app');
+
+
+describe('GET /', () => {
+    it('should respond with "Hello World"', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual('Hello World');
+    });
+});
